704d03c9fde0b54a25b0b18317480125a9cd9b78
Marcelo Magday <marcelomagday@gmail.com>
2022-10-28 21:19:00 +0800

    feat: initial design of schedule table

--- src/component/schedule/ScheduleTable.jsx
+++ src/component/schedule/ScheduleTable.jsx
@@ -1,8 +1,35 @@
+import { Table } from "antd";
+
 const ScheduleTable = () => {
+    const columns = [
+        {
+            title: 'Subject Code',
+            dataIndex: 'subjectCode',
+            key: 'subjectCode'
+        },
+        {
+            title: 'Subject Name',
+            dataIndex: 'subjectName',
+            key: 'subjectName'
+        },
+        {
+            title : 'Time',
+            dataIndex: 'timeSlot',
+            key: 'timeSlot'
+        }
+    ];
 
+    const dummyData = [
+        {
+            key: '1',
+            subjectCode: 'CSELEC06',
+            subjectName: 'Advanced Web Programming',
+            timeSlot: '2022-01-01 07:30:00+08 - 2022-01-01 11:00:00+08'
+        }
 
+    ];
     return <>
-
+        <Table columns={columns} dataSource={dummyData} pagination={false}/>
     </>;
 };
 
